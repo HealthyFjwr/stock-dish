93abf6af0efffba27f211e6a529f683c92d41978
わら <fjwr.0401@gmail.com>
2026-07-04 10:27:52 +0900

    test: add test about email format

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,9 @@ FactoryBot.define do
     username { "user01" }
     email { "test01@example.com" }
     password { "password01" }
+
+    trait :invalid_email do
+      email { "invalid_email_format" }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,22 @@ RSpec.describe User, type: :model do
         expect(user.errors[:email]).to include("を入力してください")
       end
     end
+    context "email が不正な形式である場合" do
+      it "無効である" do
+        invalid_emails = %w[
+          test01example.com
+          test01@
+          @example.com
+          test01@example
+          test01@example..com
+        ]
+        invalid_emails.each do |invalid_email|
+          user = build(:user, :invalid_email, email: invalid_email)
+          user.valid?
+          expect(user.errors[:email]).to include("は不正な値です"), "#{invalid_email} が有効と判定されました"
+        end
+      end
+    end
     context "password が空である場合" do
       it "無効である" do
         user = build(:user, password: nil)
