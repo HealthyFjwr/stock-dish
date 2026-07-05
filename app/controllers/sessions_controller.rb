e9abd33ca0efa15d35aa14d5bfd3bef98e196d27
わら <fjwr.0401@gmail.com>
2026-07-05 20:27:53 +0900

    add: Views session#new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to dashboard_path, notice: "Successfully logged in"
+    else
+      flash.now[:alert] = "Email or Password is invalid"
+      render "new", status: :unauthorized
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Successfully logged out"
+  end
 end
