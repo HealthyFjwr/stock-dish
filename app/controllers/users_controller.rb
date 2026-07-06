59c331f4e3e66daf71520ae65774d1cbaabad285
わら <fjwr.0401@gmail.com>
2026-07-07 01:16:39 +0900

    add: controller Users edit update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(update_user_params)
+      redirect_to dashboard_path, notice: "User was successfully updated."
+    else
+      flash.now[:alert] = @user.errors.full_messages.join(", ")
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def update_user_params
+    params.require(:user).permit(:username)
   end
 end
